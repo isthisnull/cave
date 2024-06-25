bf15d8fe7e3dc5035630a58e24577bae05dd85e7
isthisnull <letscodewithzanjiri@hotmail.com>
2024-06-25 21:07:43 +0300

    sss

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-"use client";
-import Row from "@/src/frontend/components/common/Row";
 import React from "react";
-import styled from "styled-components";
-const KirEl = styled(Row)`
-  font-weight: 600;
-  font-size: 3rem;
-  background-color: red;
-  transition: 0.4s ease-in;
-  &:hover {
-    transform: translateX(50px);
-  }
-`;
+import Profile from "@/src/frontend/components/Profile";
+
 function page() {
-  return <KirEl>hi</KirEl>;
+  return (
+    <>
+      <Profile />
+    </>
+  );
 }
 
 export default page;
---
+++ src/frontend/components/Profile.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Row from "@/src/frontend/components/common/Row";
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+
+const ContainerEl = styled(Row)`
+  width: 360px;
+  justify-content: center;
+  background-color: rebeccapurple;
+`;
+const ProfileEl = styled(Row)`
+  width: calc(100% - 30px);
+  justify-content: space-between;
+  background-color: red;
+`;
+const AvatarEl = styled(Row)``;
+const InfoEl = styled(Row)`
+  flex-direction: column;
+`;
+const HelloEl = styled(Row)``;
+const NameEl = styled(Row)``;
+const BellEl = styled(Row)``;
+function Profile() {
+  return (
+    <ContainerEl>
+      <ProfileEl>
+        <AvatarEl>ax</AvatarEl>
+        <InfoEl>
+          <HelloEl>Hello there,</HelloEl>
+          <NameEl>Daniel</NameEl>
+        </InfoEl>
+        <BellEl>ring</BellEl>
+      </ProfileEl>
+    </ContainerEl>
+  );
+}
+
+export default Profile;
